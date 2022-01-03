e7fd9f43f503419a570d052ec7e9bcf4de8f1081
ellacay <71969779+ellacay@users.noreply.github.com>
2022-01-03 13:48:42 +0800

    Update Home.js

--- client/src/MainPages/Structure/Home.js
+++ client/src/MainPages/Structure/Home.js
@@ -59,38 +59,43 @@ const Home = (({ history }) => {
 <p>If you teach one girl how to code, she will teach three" - Reshma Saujani, Founder of Girls Who Code</p>
 <p>Tech In With Ella is a discussion onn the technology industry. The ins and outs from the pov of a young woman. The journey taken to learn the skills to code and navigate the complications and problems that arose.</p>
 </div>
- <div className="Blogs" key="Blogs">
-
-<div className='grid'>
-
-  {filter.map((blogPosts) => (
-
-    <Card 
-    key={blogPosts._id}
-    className="card"
-    onClick={() => {
-   
-      window.location.replace("/post/" + blogPosts._id);
-  
-  }}
->
-    <CardActionArea>
-        <p    className="text" id="postTitle" >{blogPosts.title}</p>
-    <p className="text" id="postSubtitle" >{new Date(blogPosts.createdAt).toDateString()}</p>
-  <img className="image" src={PF + blogPosts.photo} alt="" />
-
-    </CardActionArea>
-  </Card>
-
-
-))}
-
-</div>
-  
-  </div>
+ 
 </div>
     
     )
 })
 
 export default Home
+
+
+
+
+// <div className="Blogs" key="Blogs">
+
+// <div className='grid'>
+
+//   {filter.map((blogPosts) => (
+
+//     <Card 
+//     key={blogPosts._id}
+//     className="card"
+//     onClick={() => {
+   
+//       window.location.replace("/post/" + blogPosts._id);
+  
+//   }}
+// >
+//     <CardActionArea>
+//         <p    className="text" id="postTitle" >{blogPosts.title}</p>
+//     <p className="text" id="postSubtitle" >{new Date(blogPosts.createdAt).toDateString()}</p>
+//   <img className="image" src={PF + blogPosts.photo} alt="" />
+
+//     </CardActionArea>
+//   </Card>
+
+
+// ))}
+
+// </div>
+  
+//   </div>
