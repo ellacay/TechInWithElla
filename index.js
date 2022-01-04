2983718f41c3d4c06aaf393c271fc2de349ef094
ellacay <71969779+ellacay@users.noreply.github.com>
2022-01-04 14:07:25 +0800

    Update index.js

--- index.js
+++ index.js
@@ -47,15 +47,13 @@ app.use("/api/subscribe", subscribeRoute);
 
 
 // set static folder
-    app.use(express.static("client/build"));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
-    if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build')); // serve the static react app
-  app.get(/^\/(?!api).*/, (req, res) => { // don't serve api routes to react app
-    res.sendFile(path.join(__dirname, './client/build/index.html'));
-  });
-  console.log('Serving React App...');
-};
 
 app.listen(PORT, () => {
   console.log('Backend is running');
